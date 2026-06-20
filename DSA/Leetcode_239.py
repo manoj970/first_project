0f5a87fc5237d5fcd1860ac4e9af891e01e94c32
manoj <manojmerugu2004@gmail.com>
2026-06-20 09:46:14 +0530

    Hard Level problem on Array using sliding window technique

---
+++ DSA/Leetcode_239.py
@@ -0,0 +1,19 @@
+from collections import deque
+def maxSlidingwindow(nums, k):
+    output = []
+    q = deque()
+    l = r = 0
+    while r < len(nums):
+        while q and nums[q[-1]] < nums[r]:
+            q.pop()
+        q.append(r)
+        if l > q[0]:
+            q.popleft()
+        if (r+1) >=k:
+            output.append(nums[q[0]])
+            l+=1
+        r +=1
+    print(output)
+maxSlidingwindow([1] , 1)                    
+
+    
